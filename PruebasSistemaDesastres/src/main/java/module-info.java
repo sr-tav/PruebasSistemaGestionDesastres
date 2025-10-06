26cb1bc1e433c1292fa6d94b18b196b6beb5eabe
Tomas Aristizabal <159397819+sr-tav@users.noreply.github.com>
2025-10-06 18:00:19 -0500

    feat: ajuste javafx

--- PruebasSistemaDesastres/src/main/java/module-info.java
+++ PruebasSistemaDesastres/src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.example.pruebassistemadesastres {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires javafx.graphics;
 
     opens com.example.pruebassistemadesastres to javafx.fxml;
     exports com.example.pruebassistemadesastres;
