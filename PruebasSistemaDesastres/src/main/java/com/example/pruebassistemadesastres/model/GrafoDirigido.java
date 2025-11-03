768505845f753da4b5db69b56da0fa97d002ff83
jfsc27 <juanf.smithc@uqvirtual.edu.co>
2025-11-02 22:39:48 -0500

    Feat: Implementacion de algoritmo de Dijkstra
    Se implementa el algortimo para calcular la ruta mas corta

--- PruebasSistemaDesastres/src/main/java/com/example/pruebassistemadesastres/model/GrafoDirigido.java
+++ PruebasSistemaDesastres/src/main/java/com/example/pruebassistemadesastres/model/GrafoDirigido.java
@@ -19,4 +19,63 @@ public class GrafoDirigido {
     public Set<Zona> zonas() {
         return adyacencia.keySet();
     }
+
+    //Dijkstra
+    public Ruta calcularRutaMasCorta(Zona origen, Zona destino) {
+
+        Map<Zona, Double> distancia = new HashMap<>();
+        Map<Zona, Zona> anterior = new HashMap<>();
+
+        for (Zona z : adyacencia.keySet()) {
+            distancia.put(z, Double.POSITIVE_INFINITY);
+        }
+        distancia.put(origen, 0.0);
+
+        PriorityQueue<Zona> cola = new PriorityQueue<>(Comparator.comparingDouble(distancia::get));
+        cola.add(origen);
+
+        while (!cola.isEmpty()) {
+            Zona actual = cola.poll();
+
+            if (actual.equals(destino)) break;
+
+            for (Camino camino : salidas(actual)) {
+                Zona vecino = camino.getHasta();
+                double nuevaDistancia = distancia.get(actual) + camino.getDistanciaKm();
+
+                if (nuevaDistancia < distancia.get(vecino)) {
+                    distancia.put(vecino, nuevaDistancia);
+                    anterior.put(vecino, actual);
+                    cola.add(vecino);
+                }
+            }
+        }
+
+        if (!anterior.containsKey(destino)) {
+            return null;
+        }
+
+        List<Zona> paradas = new ArrayList<>();
+        Zona actual = destino;
+        paradas.add(destino);
+
+        while (anterior.containsKey(actual)) {
+            actual = anterior.get(actual);
+            paradas.add(actual);
+        }
+
+        Collections.reverse(paradas);
+
+        Ruta ruta = new Ruta("Ruta más corta: " + origen.getNombre() + " → " + destino.getNombre());
+        for (Zona z : paradas) {
+            ruta.agregarParada(z);
+        }
+
+        ruta.setDistanciaTotalKm(distancia.get(destino));
+
+        double tiempoHoras = distancia.get(destino) / 50.0;
+        ruta.setDuracionHoras(tiempoHoras);
+
+        return ruta;
+    }
 }
